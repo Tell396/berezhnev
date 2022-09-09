27d83ebd452453eac6c1d3f7b059d219b3f772f5
Tell396 <vova21473@gmail.com>
2022-09-09 19:28:02 +1000

    Story #4. Update about section.

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,16 +29,8 @@ const IndexPage = ({
       <Section linebreak>
         <h2 className="text-3xl md:text-4xl mb-4 text-black dark:text-white">About</h2>
         <p className="my-2">
-          I spend most of my time as a frontend developer on Ostmodern&apos;s Skylark CMS using
-          React.js. Sometimes I use Node.js, Docker, Kubernetes and Golang and I&apos;m currently
-          experimenting with Serverless technologies on AWS and Azure.
-        </p>
-        <p className="my-2">
-          Outside of work I spend my time creating content for my blog where I discuss other
-          projects I&apos;m working on, interesting problems I&apos;ve had to solve and create
-          tutorials to educate and help others use various technologies for the first time or in a
-          more efficient manner.
-        </p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus placeat ratione possimus, maxime temporibus dolores eveniet ea sed voluptates numquam harum in velit culpa tempore corporis. Dolorum nisi iure neque?
+				</p>
 			</Section>
 
       <Section>
