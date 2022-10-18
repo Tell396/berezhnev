40ec891e0ec7ec821206a5b381c7754a515accaa
Tell396 <vova21473@gmail.com>
2022-10-18 18:59:12 +1000

    Story #11. Fix markdown.

--- lib/devto.ts
+++ lib/devto.ts
@@ -1,122 +1,130 @@
-import axios, { AxiosResponse } from 'axios'
-import moment from 'moment'
+import axios, { AxiosResponse } from "axios";
+import moment from "moment";
 
-import IArticle from '../interfaces/IArticle'
-import ICachedArticle from '../interfaces/ICachedArticle'
-import IHomePageArticles from '../interfaces/IHomePageArticles'
+import IArticle from "../interfaces/IArticle";
+import ICachedArticle from "../interfaces/ICachedArticle";
+import IHomePageArticles from "../interfaces/IHomePageArticles";
 
-import { convertMarkdownToHtml, sanitizeDevToMarkdown } from './markdown'
+import { convertMarkdownToHtml, sanitizeDevToMarkdown } from "./markdown";
 
-const username = 'tell396'
-const blogURL = 'https://dev.to/tell396/'
-const portfolioURL = 'https://dev.to/tell396/'
+const username = "tell396";
+const blogURL = "https://dev.to/tell396/";
+const portfolioURL = "https://dev.to/tell396/";
 
-const featuredBlogSlug = 'test2-3p1k'
-const featuredPortfolioSlugs = [
-    'kinoway-2phc',
-]
+const featuredBlogSlug = "test2-3p1k";
+const featuredPortfolioSlugs = ["kinoway-2phc"];
 
 // Takes a URL and returns the relative slug to your website
 export const convertCanonicalURLToRelative = (canonical: string): string => {
-    if (canonical.startsWith(portfolioURL)) {
-        return canonical.replace(portfolioURL, '')
-    }
-    return canonical.replace(blogURL, '')
-}
+  if (canonical.startsWith(portfolioURL)) {
+    return canonical.replace(portfolioURL, "");
+  }
+  return canonical.replace(blogURL, "");
+};
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 const convertDevtoResponseToArticle = (data: any): IArticle => {
-    const slug = convertCanonicalURLToRelative(data.canonical_url)
-    const markdown = sanitizeDevToMarkdown(data.body_markdown)
-    const html = convertMarkdownToHtml(markdown)
+  const slug = convertCanonicalURLToRelative(data.canonical_url);
+  const markdown = sanitizeDevToMarkdown(data.body_markdown);
+  const html = convertMarkdownToHtml(markdown);
 
-    const article: IArticle = {
-        id: data.id,
-        title: data.title,
-        description: data.description,
-        publishedAt: data.published_at,
-        devToSlug: data.slug,
-        devToPath: data.path,
-        devToURL: data.url,
-        commentsCount: data.comments_count,
-        publicReactionsCount: data.public_reactions_count,
-        positiveReactionsCount: data.positive_reactions_count,
-        coverImage: data.cover_image,
-        tags: data.tag_list,
-        canonical: data.canonical_url,
-        collectionId: data.collection_id || -1,
-        slug,
-        markdown,
-        html,
-    }
-    return article
-}
+  const article: IArticle = {
+    id: data.id,
+    title: data.title,
+    description: data.description,
+    publishedAt: data.published_at,
+    devToSlug: data.slug,
+    devToPath: data.path,
+    devToURL: data.url,
+    commentsCount: data.comments_count,
+    publicReactionsCount: data.public_reactions_count,
+    positiveReactionsCount: data.positive_reactions_count,
+    coverImage: data.cover_image,
+    tags: data.tag_list,
+    canonical: data.canonical_url,
+    collectionId: data.collection_id || -1,
+    slug,
+    markdown,
+    html,
+  };
+  return article;
+};
 
 const blogFilter = (article: IArticle): boolean => {
-		const isArticle = article.tags.indexOf("showdev")
+  const isArticle = article.tags.indexOf("showdev");
 
-		if (isArticle == -1) {
-				return true
-		} else {
-				return false
-		}		
+  if (isArticle == -1) {
+    return true;
+  } else {
+    return false;
+  }
 
-		//article.canonical.startsWith(blogURL)
-}
+  //article.canonical.startsWith(blogURL)
+};
 
 const portfolioFilter = (article: IArticle): boolean => {
-		const isPortfolio = article.tags.indexOf("showdev")
+  const isPortfolio = article.tags.indexOf("showdev");
 
-		if (isPortfolio == 0) {
-				return true
-		} else {
-				return false
-		}		
-}
-		
+  if (isPortfolio == 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 // Get all users articles from Dev.to and filter by ones with a canonical URL to your blog
 export const getAllArticles = async (): Promise<IArticle[]> => {
-    const params = { username, per_page: 1000 }
-    const headers = { 'api-key': process.env.DEVTO_APIKEY }
-    const { data }: AxiosResponse = await axios.get(`https://dev.to/api/articles/me`, {
-        params,
-        headers,
-    })
-    const articles: IArticle[] = data.map(convertDevtoResponseToArticle)
-    return articles
-}
+  const params = { username, per_page: 1000 };
+  const headers = { "api-key": process.env.DEVTO_APIKEY };
+  const { data }: AxiosResponse = await axios.get(
+    `https://dev.to/api/articles/me`,
+    {
+      params,
+      headers,
+    }
+  );
+  const articles: IArticle[] = data.map(convertDevtoResponseToArticle);
+  return articles;
+};
 
 export const getAllBlogArticles = async (): Promise<IArticle[]> => {
-    const articles = await getAllArticles()
-		//console.log(articles.filter(blogFilter))
-    return articles.filter(blogFilter)
-}
+  const articles = await getAllArticles();
+  return articles.filter(blogFilter);
+};
 
 export const getAllPortfolioArticles = async (): Promise<IArticle[]> => {
-    const articles = await getAllArticles()
-    return articles.filter(portfolioFilter)
-}
+  const articles = await getAllArticles();
+  return articles.filter(portfolioFilter);
+};
 
 export const getHomePageArticles = async (): Promise<IHomePageArticles> => {
-    const articles = await getAllArticles()
-    const [latestBlog] = articles.filter(blogFilter)
-    const [latestPortfolio] = articles.filter(portfolioFilter)
+  const articles = await getAllArticles();
+  const [latestBlog] = articles.filter(blogFilter);
+  const [latestPortfolio] = articles.filter(portfolioFilter);
 
-    const featuredBlog = articles.find(({ devToSlug }) => devToSlug === featuredBlogSlug) || null
-    const featuredPortfolio = articles
-        .filter(({ devToSlug }) => featuredPortfolioSlugs.includes(devToSlug))
-        .sort((a1, a2) => (moment(a1.publishedAt).isBefore(a2.publishedAt) ? 1 : -1))
+  const featuredBlog =
+    articles.find(({ devToSlug }) => devToSlug === featuredBlogSlug) || null;
+  const featuredPortfolio = articles
+    .filter(({ devToSlug }) => featuredPortfolioSlugs.includes(devToSlug))
+    .sort((a1, a2) =>
+      moment(a1.publishedAt).isBefore(a2.publishedAt) ? 1 : -1
+    );
 
-    return {
-        latestBlog,
-        latestPortfolio,
-        featuredBlog,
-        featuredPortfolio,
-    }
-}
+  return {
+    latestBlog,
+    latestPortfolio,
+    featuredBlog,
+    featuredPortfolio,
+  };
+};
 
 // Gets an article from Dev.to using the ID that was saved to the cache earlier
-export const getArticleFromCache = (cache: ICachedArticle[], slug: string): IArticle => {
-    const article = cache.find((cachedArticle) => cachedArticle.slug === slug) as IArticle
-    return article
-}
+export const getArticleFromCache = (
+  cache: ICachedArticle[],
+  slug: string
+): IArticle => {
+  const article = cache.find(
+    (cachedArticle) => cachedArticle.slug === slug
+  ) as IArticle;
+  return article;
+};
